9e04dd7747fafce06c2445622a4564ef95188ee6
Aleksi Ahvenainen <Aleksi.Ahvenainen@student.lab.fi>
2023-01-17 14:25:05 +0200

    Company luokan lisääminen

---
+++ CompanyExercise/Company.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyExercise
+{
+    internal class Company
+    {
+        string title;
+        string address;
+        string phone;
+        int outcome;
+        int expense;
+        public Company()
+        {   
+            this.title = string.Empty;
+            this.address = string.Empty;
+            this.phone = string.Empty;
+            this.outcome = 0;
+            this.expense = 0;
+        }
+
+        public Company(string title, string address, string phone, int outcome, int expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+
+        public Company(Company other)
+        {
+            this.title = other.title;
+            this.address = other.address;
+            this.phone = other.phone;
+            this.outcome = other.outcome;
+            this.expense = other.expense;
+        }
+
+        public void Voitot()
+        {
+            int voitto = (this.outcome - this.expense) / this.expense * 100;
+
+            if (voitto < 100)
+            {
+                Console.WriteLine("Kehnosti menee. Voitto: " + voitto.ToString());
+            }
+            else if (voitto >= 100 && voitto < 200)
+            {
+                Console.WriteLine("Välttävästi menee. Voitto: " + voitto.ToString());
+            }
+            else if (voitto >= 200 && voitto < 300)
+            {
+                Console.WriteLine("Tyydyttävästi menee. Voitto: " + voitto.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Hyvin menee. Voitto: " + voitto.ToString());
+            }
+        }
+
+        internal void setTitle(string value)
+        {
+            this.title = value;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Yrityksen nimi: {0}", title);
+        }
+        public override string? ToString()
+        {
+            return "title: " + this.title + ", address: " + this.address + ", phone: " 
+                + this.phone + ", outcome: " + this.outcome + ", expense: " +  this.expense;
+        }
+    }
+}
---
+++ CompanyExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompanyExercise;
+
+Console.WriteLine("This is Company Exericse!");
+Console.WriteLine("-------------------------\n");
+
+Company finnair = new Company("Finnair Oyj", "Kalevankatu 4", "01122 22", 3000, 3000);
+//finnair.setTitle("Finnair Oyj");
+finnair.PrintInfo();
+Console.WriteLine(finnair.ToString());
+finnair.Voitot();
+Console.WriteLine("");
+
+Company lufthansa = new Company("Lufthansa Group", "Mannerheimintie 50", "010 12345", 5000000, 2500000);
+//lufthansa.setTitle("Lufthansa Group");
+lufthansa.PrintInfo();
+Console.WriteLine(lufthansa.ToString());
+lufthansa.Voitot();
+Console.WriteLine("");
+
+//kopio
+Company finavia = new Company(lufthansa);
+finavia.PrintInfo();
+Console.WriteLine(finavia.ToString());
+finavia.Voitot();
